cb0a4491b7304ab241b7b1e8cff97bf221871165
sajid <sajidahmed.77@gmail.com>
2020-01-20 01:03:37 +0300

    Add project files.

---
+++ JWTInDotNetCore/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("token")]
+       public ActionResult GetToken()
+        {
+            IdentityModelEventSource.ShowPII = true;
+            //Security Key
+            string securityKey = "This_is_the_security_key_please_replace_this_with_your_security_key";
+
+            //Symmetric Security Key
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+
+            //Signing Credentials
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            //Claims
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+            //Create Token
+            var token = new JwtSecurityToken(issuer: "niksaj", audience: "readers", expires: DateTime.Now.AddHours(1),signingCredentials:signingCredentials,claims:claims);
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+        }
+    }
+}
