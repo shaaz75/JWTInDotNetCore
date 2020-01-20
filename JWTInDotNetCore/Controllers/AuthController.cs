d9a5320a9c7e4831b7b7b018289d9f8480c9de0a
sajid <sajidahmed.77@gmail.com>
2020-01-20 09:58:57 +0300

    .

--- JWTInDotNetCore/Controllers/AuthController.cs
+++ JWTInDotNetCore/Controllers/AuthController.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Net;
+using System.IO;
 
 namespace JWT.Controllers
 {
